ad34003a854d9132b76d3ba7c0779118ab5e51a0
Georgii Gorbachev <georgii-egor.gorbachev@deliveryhero.com>
2016-03-11 03:10:23 +0300

    Build container image and push it to Quay registry

---
+++ create-gcloud-playground-controller.json.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat > gcloud-playground-controller.json <<EOF
+{
+  "kind": "ReplicationController",
+  "apiVersion": "v1beta3",
+  "metadata": {
+    "name": "playground",
+    "labels": {
+      "name": "playground"
+    }
+  },
+  "spec": {
+    "replicas": 3,
+    "selector": {
+      "name": "playground"
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "name": "playground",
+          "deployment": "${WERCKER_GIT_COMMIT}"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "imagePullPolicy": "Always",
+            "image": "quay.io/gg_dh/gcloud-playground:${WERCKER_GIT_COMMIT}",
+            "name": "playground",
+            "ports": [
+              {
+                "name": "http-server",
+                "containerPort": 8080,
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
